c766cd7c506da501e9bd8478b9ceebfa032f6a38
Darsh Mistry <1111darsh@gmail.com>
2021-04-09 15:11:41 +0530

    Add cources

--- script/courses.js
+++ script/courses.js
@@ -1,11 +1,22 @@
 var COURSES = `{
     "courses":
     [
-        {"link" : "https://1111darsh.com", "Name" : "course1"},
-        {"link" : "https://1111darsh.com", "Name" : "course2"}
+        {"link" : "https://www.udemy.com/course/successful-cross-cultural-management/?ranMID=39197&ranEAID=bnwWbXPyqPU&ranSiteID=bnwWbXPyqPU-MX1VkOpVD0qEXpkfL5QvYg&LSNPUBID=bnwWbXPyqPU&utm_source=aff-campaign&utm_medium=udemyads&couponCode=APR2021",
+         "Name" : "Successful cross-cultural management"},
+  
+        {"link" : "https://www.udemy.com/course/network-protocol-analysis-using-wireshark-part-1/?couponCode=VINOAPR3",
+         "Name" : "Network Protocol Analysis Using Wireshark Part-1"},
+        {"link" : "https://www.udemy.com/course/learn-website-design/?ranMID=39197&ranEAID=bnwWbXPyqPU&ranSiteID=bnwWbXPyqPU-Eu8Xq38ZsL1vua5lN.g7bQ&LSNPUBID=bnwWbXPyqPU&utm_source=aff-campaign&utm_medium=udemyads&couponCode=3DAYSFREE", 
+        "Name" : "Professional Website Design + WordPress Website Development"}
+        
     ]
 }`
+/*
 
+{"link" : "", 
+"Name" : ""}
+
+*/
 var courses = document.getElementById("courses");
 var json = JSON.parse(COURSES)
 for (i in json.courses) {
