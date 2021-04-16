a466c782e32911f23a6d87a78294701f237b7674
Darsh Mistry <1111darsh@gmail.com>
2021-04-16 18:03:39 +0530

    Add index before courses

--- script/courses.js
+++ script/courses.js
@@ -212,11 +212,11 @@ var COURSES = `
 var courses = document.getElementById("courses");
 var json = JSON.parse(COURSES)
 for (i in json.courses) {
-    create_course(json.courses[i].link, json.courses[i].Name, json.courses[i].rating, json.courses[i].language );
+    create_course(json.courses[i].index ,json.courses[i].link, json.courses[i].Name, json.courses[i].rating, json.courses[i].language );
 }
 
 
-function create_course(course_link, Course_Name, rating, language) {
+function create_course(index,course_link, Course_Name, rating, language) {
 
     var card = document.createElement("div");
     var att = document.createAttribute("class");
@@ -228,9 +228,12 @@ function create_course(course_link, Course_Name, rating, language) {
     var as = document.createElement("a")
     var link = document.createAttribute("href")
     as.style = "text-decoration: none"
+    var index = document.createTextNode(index+". ")
     link.value = course_link
     as.setAttributeNode(link)
     card.appendChild(as)
+    
+
 
 
     var h2 = document.createElement("h2");
@@ -238,6 +241,7 @@ function create_course(course_link, Course_Name, rating, language) {
     var att1 = document.createAttribute("class");
     att1.value = " w3-text-grey w3-padding-16"
     var coursename = document.createTextNode(Course_Name)
+    h2.appendChild(index)
     h2.appendChild(coursename)
     h2.setAttributeNode(att1)
     as.appendChild(h2);
