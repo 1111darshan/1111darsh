de0a431f0390df12adf4c4c3bb85983d86de22f3
Darsh Mistry <1111darsh@gmail.com>
2023-12-03 16:41:34 +0530

    make feedback libe

--- script/feedback.js
+++ script/feedback.js
@@ -13,7 +13,7 @@ function sendMessage() {
         message: userMessage
     };
 
-    fetch('http://127.0.0.1:5000/api/feedback', {
+    fetch('https://feedback-production-76e1.up.railway.app/api/feedback', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -26,14 +26,13 @@ function sendMessage() {
                 throw new Error('Network response was not ok');
             }
             // Do something after successful post, like displaying a success message
-            // document.getElementById('messageBox').style.display = 'none';
-            // document.getElementById('thanksMessage').style.display = 'block';
+             console.log(response.status);
+             document.getElementById('messageBox').style.display = 'none';
+             document.getElementById('thanksMessage').style.display = 'block';
         })
         .catch(error => {
             // Handle errors
             console.error('There was an error with the POST request:', error);
             // Display an error message to the user, or retry the request, etc.
         });
-    document.getElementById('messageBox').style.display = 'none';
-    document.getElementById('thanksMessage').style.display = 'block';
 }
